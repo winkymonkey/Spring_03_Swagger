36326c6214e87c2de7d268794bd085fbcb3b1187
winkymonkey <pal.abhishek.2008@gmail.com>
2021-11-29 23:25:10 +0530

    package update

--- src/main/java/org/example/spring/Student.java
+++ src/main/java/org/example/spring/Student.java
@@ -1,4 +1,4 @@
-package com.example.spring;
+package org.example.spring;
 
 public class Student {
 	private String name;
--- src/main/java/org/example/spring/SwaggerConfig.java
+++ src/main/java/org/example/spring/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.spring;
+package org.example.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
